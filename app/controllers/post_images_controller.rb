7cbd2ce6e536ca2c4c70ef84fae298e0a06aa3fc
EC2 Default User <ec2-user@ip-172-31-42-242.ap-northeast-1.compute.internal>
2023-01-30 01:00:56 +0000

    [Finish]4-11

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,9 +1,9 @@
 class PostImagesController < ApplicationController
-  
+
   def new
-    @post_image = PostimagesController
+    @post_image = PostImage.new
   end
-  
+
    # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
@@ -13,6 +13,7 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
@@ -21,13 +22,13 @@ class PostImagesController < ApplicationController
   def destroy
   end
 
-  
+
   # 投稿データのストロングパラメータ
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
-  
+
+
 end
